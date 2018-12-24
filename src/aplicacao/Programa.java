ee026d26b17c88a61bdb15791775435ae70ca6c6
Lucas Andre <lucaslopes05@gmail.com>
2018-12-24 10:17:12 -0200

    Implementado, tabuleiro, peça e posiçao

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,9 +1,12 @@
 package aplicacao;
 
+import tabuleiroDoXadrez.Tabuleiro;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 		
 	}
 }
---
+++ src/tabuleiroDoXadrez/Peca.java
@@ -0,0 +1,21 @@
+package tabuleiroDoXadrez;
+
+public class Peca {
+
+	protected Posicao posicao;
+	protected Tabuleiro tabuleiro;
+	
+	public Peca(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
+		posicao = null;
+	}
+
+	public Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
+
+	
+	
+	
+	
+}
